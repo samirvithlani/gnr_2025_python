29384662a1bedda5cc72962b99b93998b337df9a
samirvithlani <samir.vithlani83955@gmail.com>
2025-04-19 15:04:56 +0530

    basics

---
+++ basics/basic1.py
@@ -0,0 +1,33 @@
+#printf() -> function
+#cout : oject | referace outputstream
+#print()
+#print will give you \n default..
+print("Hello")
+print("Hello",end=",")
+print("ok") #indentation
+
+a=100 #int
+#print(a)
+print("value of a = ",a)
+print("type of a = ",type(a))
+
+pers = 78.90 #float
+print("pers = ",pers)
+print("type of pers = ",type(pers))
+
+x1 ='y' #str #string.
+print("x1 = ",x1)
+print("type of x1 = ",type(x1))
+
+name = "royal"
+print("name = ",name)
+print("type of name = ",type(name))
+
+isActive = True #bool
+print("isACtive ? ",isActive)
+print("type of isactive",type(isActive))
+
+data = None #NoneType
+print("data = ",data)
+print("type of data = ",type(data))
+
---
+++ basics/basic2.py
@@ -0,0 +1,20 @@
+#scanf
+#cin>> inpuut
+#input()
+#return type of input is string...
+# no1 =input("enter no1") #"100"
+# print("no1 = ",no1)
+# print("type of no1 = ",type(no1))
+
+
+#int x = 100.20
+no1 = int(input("enter no1"))
+print("no1 = ",no1) 
+print("type of no1 = ",type(no1))
+
+pers = float(input("please enter pers.."))
+print("pers = ",pers)
+print("type of pers = ",type(pers))
+
+name =input("enter name")
+print("name =",name)
