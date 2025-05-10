a4b06119549508a08e3c626606f775aaf124980a
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-10 15:02:13 +0530

    string

---
+++ string/string2.py
@@ -0,0 +1,9 @@
+data= "hi this is stirng"
+space=0
+for i in range(0,len(data)):
+    if data[i]==" ":
+        space+=1
+
+print("number of space in string is",space)        
+print("nummer pf words in string is",space+1)
+    
