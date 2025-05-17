bfc351efc0043833faa8e71a7eab6bc0d7acb288
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-17 14:22:33 +0530

    klist

---
+++ list/ListDemo1.py
@@ -0,0 +1,24 @@
+data = [] #empty list..
+print(data)
+print(type(data))
+
+users = ["ram","shyam","amit","sumit"]
+print(users)
+# print(users[0])
+# print(users[1])
+# print(users[2])
+# print(users[3])
+
+#len..
+size = len(users)
+print("size",size)
+
+# for i in range(0,len(users)):
+#     print(users[i])
+
+
+# for i in users:
+#     print(i)
+users[0] = "RAM"
+print(users)
+
