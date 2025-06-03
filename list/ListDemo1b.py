5c14ff2faabe76f0e00145e29f3aeafea31b186e
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-03 17:30:53 +0530

    list

---
+++ list/ListDemo1b.py
@@ -0,0 +1,22 @@
+# users =[] #empty list
+# print(users)
+# print(type(users))
+# print(len(users))
+
+users = ["ram","shyam","amit","sumit","seeta"]
+print(users)
+
+#print(users[1])
+# for i in range(0,len(users)):
+#     print(users[i])
+
+# for i in users:
+#     print(i)
+
+users.append("janki")
+users.append("laxmi")
+
+#users.extend(["arjun","sahdev"])
+#users.extend("sahdev")
+users.insert(2,"bhim")
+print(users)
